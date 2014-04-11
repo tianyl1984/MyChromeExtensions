f04c20c5da9210f48c129fbecfa206fed2ed7b2d
tianyl1984 <tianyl1984@qq.com>
2014-04-11 14:53:06 +0800

    修改google搜索结果跳转方式

--- js/googleSearch.js
+++ js/googleSearch.js
@@ -1,8 +1,25 @@
 if(window.location.href.indexOf("https://www.google.com.hk/") != -1){
 	//禁止转向google网站，经常无法访问
-	$(".r").live("mouseover",function(e){
-		$(this).find("a").removeAttr("onmousedown");
-	});
+//	$(".g").live("mouseover",function(e){
+//		var $a = $(this).find("a:eq(0)");
+//		if($a.attr("data-href")){
+//			$a.attr("href",$a.attr("data-href"));
+//			$a.removeAttr("data-href");
+//		}
+//		//$a.hide();
+//		$a.removeAttr("onmousedown");
+//		$a.unbind("mousedown");
+//		
+//		$a.bind("mousedown",function(evt){
+//			evt.stopPropagation();
+//			evt.preventDefault();
+//		});
+//	});
+	
 }else{
 	//alert("bad");
 }
+
+//chrome.tabs.onUpdated.addListener(function(tabId,changeInfo,tab){
+//	alert(changeInfo.url);
+//});
