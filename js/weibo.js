6a8b4cbed486e2ee7677e04d153722fcb7150dc7
tianyl1984 <tianyl1984@qq.com>
2015-02-21 08:05:57 +0800

    修改weibo的链接，直接打开目标地址，不经过短网址跳转

--- js/weibo.js
+++ js/weibo.js
@@ -1,7 +1,29 @@
 if(window.location.href.indexOf("weibo.com") != -1){
-	document.addEventListener("DOMNodeInserted", function(ele) {
-		ele = ele.target.parentNode;
-		$(".WB_feed_type[feedtype=ad]",$(ele)).hide();
-		$("div[node-type='feed_spread']",$(ele)).hide();
+	var timeoutId = null;
+	document.addEventListener("DOMNodeInserted", function(event) {
+		if(timeoutId != null){
+			window.clearTimeout(timeoutId);
+			timeoutId = null;
+		}
+		timeoutId = setTimeout(function(){
+			procAHref();
+		},2000);
+		
 	}, false);
 }
+
+//处理weibo中的链接，直接到目的地址，不经过短网址跳转
+function procAHref(){
+	$("a[action-type=feed_list_url]").each(function(){
+		var $a = $(this);
+		if($a.attr("href").indexOf("t.cn") != -1){
+			if($a.attr("title").indexOf("http") == 0){
+				$a.attr("action-type","feed_list_url_bak");
+				$a.attr("href-bak",$a.attr("href"));
+				$a.attr("href",$a.attr("title"));
+			}
+		}
+	});
+//	console.log("procAHref:" + .size())
+}
+
